9259e4c87c75dc46d84e649384bfead6676dab2d
Danny <contact@danfoster.io>
2018-01-19 16:16:33 -0600

    random-word_generator final

---
+++ apps/word_generator/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.utils.crypto import get_random_string
+
+def index(request):
+    if 'counter' not in request.session:
+        request.session['counter'] = 0
+    return render(request, 'word_generator/index.html')
+
+def generate(request):
+    request.session['random_word'] = get_random_string(length=13)
+    request.session['counter'] += 1
+    return render(request, 'word_generator/index.html', {'random_word': request.session['random_word'], 'counter': request.session['counter']})
+
+def reset(request):
+    del request.session['counter']
+    return redirect('/')
